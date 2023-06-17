227615f87509a559fee10244dd6e461b6f7ee388
AsPulse <84216737+AsPulse@users.noreply.github.com>
2023-06-17 22:11:53 +0900

    Add Disk

---
+++ Assets/Scripts/GameLogic/ReversiDisk.cs
@@ -0,0 +1,29 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+public class ReversiDisk : UdonSharpBehaviour
+{
+    [SerializeField]
+    private float Radius;
+
+    private bool _color;
+
+    public bool Color {
+        get => _color;
+        set {
+            animator.SetBool("Color", value);
+            _color = value;
+        }
+    }
+
+    private GameObject ReversiLogic;
+    private Animator animator;
+
+    public void Setup(GameObject logic, bool initialColor)
+    {
+        animator = GetComponent<Animator>();
+        ReversiLogic = logic;
+        Color = initialColor;
+        transform.parent = logic.transform;
+    }
+}
---
+++ Assets/Scripts/GameLogic/ReversiLogic.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ReversiLogic : UdonSharpBehaviour
+{
+    [SerializeField]
+    private GameObject DiskPrefab;
+
+    void Start()
+    {
+        GameObject test = Instantiate(DiskPrefab);
+        test.GetComponent<ReversiDisk>().Setup(gameObject, true);
+    }
+}
